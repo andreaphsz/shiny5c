36e7be4ef8b54498055b2bded7d600c53adf994f
Andrea Cantieni <andrea.cantieni@phsz.ch>
2017-02-21 14:28:38 +0100

    scatterplot with mouse-over

--- server.R
+++ server.R
@@ -1,5 +1,4 @@
 library(shiny)
-library(leaflet)
 library(googleVis)
 library(ggplot2)
 library(dplyr)
@@ -8,50 +7,28 @@ library(tidyr)
 source("global.R")
 
 shinyServer(function(input, output, session) {
-  
-  output$mymap <- renderLeaflet({
-    leaflet() %>%
-      addProviderTiles("Stamen.TonerLite",
-                       options = providerTileOptions(noWrap = TRUE)
-      ) %>%
-      setView( 9.838323, 46.497777, 2)
-    #addTiles() 
-    #addMarkers(data = points())
-  })
-  
-  #proj <- c("mercator", "albers", "lambert","kavrayskiy-vii")
-  
+   
   output$gvismap <- renderGvis({
     gvisGeoChart(data.all, locationvar="Country_Name", 
-                 colorvar = input$factor,
+                 colorvar = input$facdim1,
                  options = list(projection = input$projection,
                               width = 900, height = 500,
                               region = input$region, gvis.editor="test"))
   })
 
-#  output$regionsel <- renderUI({
-#    region <- c("World" = "world",
-#      "Africa"= "002", 
-#      "Europe"="150",
-#      "Americas"="019",
-#      "Asia"="142",
-#      "Oceania"="009")
-#    selectInput("regionsel","Region", region, "world")
-#  })
-  
-  output$csel <- renderUI({
-    selectInput("csel","Countries", data.all$Country_Name, "Switzerland", multiple = TRUE)
+  output$country2 <- renderUI({
+    selectInput("country2","Countries", data.all$Country_Name, "Switzerland", multiple = TRUE)
   })
 
-  output$dsel <- renderUI({
-    selectInput("dsel","Dimensions", paste0("Dimension_",1:7), "Dimension_1", multiple = TRUE)
+  output$dim2 <- renderUI({
+    selectInput("dim2","Dimensions", paste0("Dimension_",1:7), "Dimension_1", multiple = TRUE)
   })
   
-  output$dims <- renderPlot({
-    if(!is.null(input$dsel)) {
+  output$dimplot <- renderPlot({
+    if(!is.null(input$dim2)) {
       data <- data.all %>% 
-        filter(Country_Name %in% input$csel) %>%
-        select_("Country_Name", .dots=input$dsel) %>%
+        filter(Country_Name %in% input$country2) %>%
+        select_("Country_Name", .dots=input$dim2) %>%
         gather(Dimension, Value, contains("Dimension"))
     
       p <- ggplot(data, aes(Dimension, Value, fill=Country_Name))
@@ -59,7 +36,49 @@ shinyServer(function(input, output, session) {
     }
   })
   
-  output$test <- renderPrint({
-    input$dsel
+  output$dim3 <- renderUI({
+    selectInput("dim3","Dimension", paste0("Dimension_",1:7), "Dimension_1")
   })
+
+  output$fac3 <- renderUI({
+    selectInput("fac3","Factor", paste0("Factor_",1:10), "Factor_1")
+  })
+  
+  output$size3 <- renderUI({
+    selectInput("size3","Size", paste0("Factor_",1:10), "Factor_2")
+  })
+  
+  output$dimxfac <- renderPlot({
+    #data <- data.all %>%
+    #  select_(.dots=c(input$dim3, input$fac3))
+    
+    p <- ggplot(data.all, aes_string(input$dim3, input$fac3, size=input$size3))
+    p + geom_point()
+  })
+  
+  ## see https://gitlab.com/snippets/16220
+  output$hover_info <- renderUI({     
+    hover <- input$plot_hover     
+    point <- nearPoints(data.all, hover, threshold = 5, maxpoints = 1, addDist = TRUE)     
+    if (nrow(point) == 0) return(NULL)         
+    ## calculate point position INSIDE the image as percent of total dimensions     
+    ## from left (horizontal) and from top (vertical)     
+    left_pct <- (hover$x - hover$domain$left) / (hover$domain$right - hover$domain$left)     
+    top_pct <- (hover$domain$top - hover$y) / (hover$domain$top - hover$domain$bottom)         
+    ## calculate distance from left and bottom side of the picture in pixels     
+    left_px <- hover$range$left + left_pct * (hover$range$right - hover$range$left)     
+    top_px <- hover$range$top + top_pct * (hover$range$bottom - hover$range$top)         
+    ## create style property fot tooltip     
+    ## background color is set so tooltip is a bit transparent     
+    ## z-index is set so we are sure are tooltip will be on top     
+    style <- paste0("position:absolute; z-index:100; background-color: rgba(245, 245, 245, 0.85);",                     "left:", left_px + 2, "px; top:", top_px + 2, "px;")         
+    ## actual tooltip created as wellPanel     
+    wellPanel(style = style, HTML(paste0(point$Country_Name, "<br>",
+                                         "Dim: ", round(point[,input$dim3],2), "<br>",
+                                         "Fac: ", round(point[,input$fac3],2), "<br>",
+                                         "Size: ", round(point[,input$size3],2)))
+    )   
+  })
+  
 })
+
--- ui.R
+++ ui.R
@@ -8,16 +8,9 @@ regions <- c("World" = "world",
              "Asia"="142",
              "Oceania"="009")
 
-factors <- c("Factor 1" = "Factor_1",
-             "Factor 2" = "Factor_2",
-             "Factor 3" = "Factor_3",
-             "Factor 4" = "Factor_4",
-             "Factor 5" = "Factor_5",
-             "Factor 6" = "Factor_6",
-             "Factor 7" = "Factor_7",
-             "Factor 8" = "Factor_8",
-             "Factor 9" = "Factor_9",
-             "Factor 10" = "Factor_10")
+factors <- paste0("Factor_", 1:10)
+
+dimensions <- paste0("Dimension_", 1:7)
 
 projections <- c("mercator", "albers", "lambert","kavrayskiy-vii")
   
@@ -30,19 +23,31 @@ shinyUI(fluidPage(
       ),
       conditionalPanel(condition="input.conditionedPanels==1",
         selectInput("region", "Region:", regions),
-        selectInput("factor", "Factor:", factors),
+        #selectInput("factor", "Factor:", factors),
+        selectInput("facdim1", "Factor/Dimension", c(factors, dimensions)),
         selectInput("projection", "Map Projection:", projections)
       ),
       conditionalPanel(condition="input.conditionedPanels==2",
-        uiOutput("csel"), 
-        uiOutput("dsel")
+        uiOutput("country2"), 
+        uiOutput("dim2")
+      ),
+      conditionalPanel(condition="input.conditionedPanels==3",
+        uiOutput("dim3"),
+        uiOutput("fac3"),
+        uiOutput("size3")
       )
     ),
     mainPanel(
       tabsetPanel(
         tabPanel("Maps", htmlOutput("gvismap"), icon = icon("globe"), value=1),
-        tabPanel("Dimensions",  plotOutput("dims"), value=2),
-        tabPanel("test", verbatimTextOutput("test")),
+        tabPanel("Dimensions",  plotOutput("dimplot"), value=2),
+        tabPanel("Dim x Factor", div(       
+          style = "position:relative",       
+          plotOutput("dimxfac",                  
+                     hover = hoverOpts("plot_hover", 
+                                       delay = 100, delayType = "debounce")),       
+          uiOutput("hover_info")     
+          ), value=3   ),
         id = "conditionedPanels"
       )
     )
