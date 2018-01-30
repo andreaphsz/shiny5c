7187a6e664ea8c6367a290cbb666695d57820c2f
andreaphsz <andrea.cantieni@phsz.ch>
2018-01-30 16:32:20 +0100

    branding

--- ui.R
+++ ui.R
@@ -22,11 +22,10 @@ shinyUI(fluidPage(
         tags$style(type='text/css', ".well { background-color: #fafafa; }"),
         tags$style(type='text/css', ".multi  { border-color: #aaa; }"),
         tags$style(type='text/css', ".single { border-color: #aaa; }"),
-        tags$style(type='text/css', "h2 { color:red; font-size:0px; background-size: 100px auto;
-                   background-image: url(5clogo.jpg); background-repeat: no-repeat;margin: auto; height:75px;}"),
         tags$style(type='text/css', "#dimplot {height: 500px !important;}"),
         tags$style(type='text/css', "#dimxfac {height: 500px !important;}"),
-        includeScript("www/ga.js")
+        includeScript("www/ga.js"),
+        includeScript("www/5c.js")
       ),
       conditionalPanel(condition="input.conditionedPanels==1",
         selectInput("region", "Region", regions),
---
+++ www/5c.js
@@ -0,0 +1,4 @@
+$( document ).ready(function() {
+    $( "h2" ).replaceWith( ('<div style="padding-bottom:77px;width:90\%;"><a href="https://5c.careers/" target="_blank"><img src="5clogo.jpg" height="75" align="left"></a><a href="https://www.wu.ac.at/" target="_blank"><img src="wuwien.png" height="60" align="right" style="margin-top:10px"></a><a href="https://www.phsz.ch/" target="_blank"><img  src="phsz.jpg" height="30" align="right" style="margin-top:10px;margin-right:30px"></a></div>') );
+});
+
