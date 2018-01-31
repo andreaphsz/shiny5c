a6ece70656e46a326c942251bb1fad0cd27cf157
andreaphsz <andrea.cantieni@phsz.ch>
2018-01-31 09:43:34 +0100

    optimized branding

--- www/5c.js
+++ www/5c.js
@@ -1,4 +1,26 @@
 $( document ).ready(function() {
-    $( "h2" ).replaceWith( ('<div style="padding-bottom:77px;width:90\%;"><a href="https://5c.careers/" target="_blank"><img src="5clogo.jpg" height="75" align="left"></a><a href="https://www.wu.ac.at/" target="_blank"><img src="wuwien.png" height="60" align="right" style="margin-top:10px"></a><a href="https://www.phsz.ch/" target="_blank"><img  src="phsz.jpg" height="30" align="right" style="margin-top:10px;margin-right:30px"></a></div>') );
+    var divtab = '<div style="width: 100%; display: table;">' +
+	'<div style="display: table-row">'
+    
+    var fiveclogo = '<div style="width: 270px; display: table-cell;position:relative;float:left">'+
+	'<a href="https://5c.careers/" target="_blank">' +
+	'<img src="5clogo.jpg" height="75" align="left"></a></div>'
+
+    var phszlogo = '<a href="https://www.phsz.ch/" target="_blank">' +
+	'<img  src="phsz.jpg" height="30" align="right" style="margin-top:10px;margin-right:30px"></a></div>'
+
+    var wulogo = '<div style="display: table-cell;position:relative;float:left;width:66\%">' +
+	'<a href="https://www.wu.ac.at/" target="_blank">' +
+	'<img src="wuwien.png" height="60" align="right" style="margin-top:10px"></a>'
+
+    var divend = '</div></div>'
+
+    $( "h2" ).replaceWith( divtab+fiveclogo+wulogo+phszlogo+divend );
 });
+
+
+
+//style="padding-bottom:77px; max-width:270px;"
+//'<div style="padding-bottom:77px;width:90\%;">
+
 
