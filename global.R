41df718d729deae00b2824d4e7dcf547a6d806c6
andreaphsz <andrea.cantieni@phsz.ch>
2018-03-09 13:47:24 +0100

    variable definitions

--- global.R
+++ global.R
@@ -4,53 +4,52 @@ data.all <- readRDS("data_mrg.rda")
 ## define global variables
 i.list <- list(
     "Financial Success" = "fsu_i",
-    "Entrepreneurship" = "es_i",
+    "Entrepreneurship" = "ent_i",
     "Positive Work Relationships" = "pwr_i",
-    "Positive Impact" = "pi_i",
-    "Learning and Development" = "ld_i",
+    "Positive Impact" = "pim_i",
+    "Learning and Development" = "lde_i",
     "Work-Life Balance" = "wlb_i",
     "Financial Security" = "fse_i")
 
 a.list <- list(
     "Financial Success" = "fsu_a",
-    "Entrepreneurship" = "es_a",
+    "Entrepreneurship" = "ent_a",
     "Positive Work Relationships" = "pwr_a",
-    "Positive Impact" = "pi_a",
-    "Learning and Development" = "ld_a",
+    "Positive Impact" = "pim_a",
+    "Learning and Development" = "lde_a",
     "Work-Life Balance" = "wlb_a",
     "Financial Security" = "fse_a")
 
 gap.list <- list(
     "Financial Success" = "fsu_gap",
-    "Entrepreneurship" = "es_gap",
+    "Entrepreneurship" = "ent_gap",
     "Positive Work Relationships" = "pwr_gap",
-    "Positive Impact" = "pi_gap",
-    "Learning and Development" = "ld_gap",
+    "Positive Impact" = "pim_gap",
+    "Learning and Development" = "lde_gap",
     "Work-Life Balance" = "wlb_gap",
     "Financial Security" = "fse_gap")
 
 ind.list <- list(
-    "WorkSuc" = "WorkSuc",
-    "Occchange" = "Occchange",
-    "Empchange" = "Empchange",
-    "Promotion" = "Promotion",
-    "CareerBeh" = "CareerBeh",
-    "IntQuit" = "IntQuit",
-    "Supervisor" = "Supervisor",
-    "OrgCom" = "OrgCom",
-    "Employ" = "Employ",
-    "Orginvest" = "Orginvest",
-    "LifeSat" = "LifeSat",
-    "Health" = "Health",
-    "Educ" = "Educ"
+    "Overall Career Success" = "carsuc",
+    "No. of Occupations" = "no_occ",
+    "No. of Employers" = "no_emp",
+    "No. of Promotions" = "no_prom",
+    "Career Aspirations" = "carasp",
+    "Turnover Intention" = "turnint",
+    "Supervisor Support" = "svsupp",
+    "Affective Commitment" = "affcom",
+    "Employability" = "employ",
+    "Employee development" = "pied",
+    "Life Satisfaction" = "lifesat",
+    "Health" = "health"
 )
 
 cnt.list <- list(
-    "GDP" = "GDP",
-    "Global Competitiveness Score" =  "GCS",
-    "Post-transfer Gini" =  "PGini",
-    "Poverty rate" = "Pov",
-    "Education/Skills" = "Edu"
+    "GDP" = "gdp",
+    "Global Competitiveness Score" =  "gcs",
+    "Gini Coefficient" =  "gini",
+    "Poverty rate" = "povr",
+    "Education/Skills" = "educ"
 )
 
 all.list <- list(i.list, a.list, gap.list, ind.list, cnt.list)
